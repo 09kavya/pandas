f24491e951d378de2a517d7b537205a226510c39
Kavya Negi <negikavya09@gmail.com>
2026-09-19 22:55:31 +0530

    modify graph ploting

--- series/graph ploting/movies.py
+++ series/graph ploting/movies.py
@@ -3,5 +3,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 a=pd.read_csv(r"C:\Users\Lenovo\Downloads\bollywood.csv",index_col="movie")
-b=a.value_counts().head(20).plot(kind='pie')
+b=a.value_counts().head(20).plot(kind='bar')
 plt.show()
