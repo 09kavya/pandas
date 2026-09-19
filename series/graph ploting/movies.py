40438c65126b218aea217c72d5e195324ce1f0cb
Kavya Negi <negikavya09@gmail.com>
2026-09-19 18:07:25 +0530

    graph ploting

---
+++ series/graph ploting/movies.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+a=pd.read_csv(r"C:\Users\Lenovo\Downloads\bollywood.csv",index_col="movie")
+b=a.value_counts().head(20).plot(kind='pie')
+plt.show()
---
+++ series/graph ploting/subs.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+a=pd.read_csv(r'C:\Users\Lenovo\Downloads\subs.csv')
+a.plot()
+plt.show()
