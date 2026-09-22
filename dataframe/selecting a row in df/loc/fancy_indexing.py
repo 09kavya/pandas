55fc25d2407eb00d1a7234f21b946591fa63bf8a
Kavya Negi <negikavya09@gmail.com>
2026-09-23 00:02:56 +0530

    loc

---
+++ dataframe/selecting a row in df/loc/fancy_indexing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+student_dict={
+    "name":["nitish","abhay","sagar","pooja"],
+    'iq':[100,90,120,80],
+    'marks':[80,70,100,50],
+    'package':[10,7,14,2]
+}
+
+a=pd.DataFrame(student_dict)
+b=a.set_index("name")
+print(b.loc[['nitish',"sagar","pooja"]])
